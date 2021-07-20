d98698bdf28eb4dca5ad837ae42c45176960c7ba
Ayman <84075654+ayman-musk@users.noreply.github.com>
2021-07-21 02:09:16 +0530

    Update time.c

--- time.c
+++ time.c
@@ -1,14 +1,11 @@
- #include<stdio.h>
-
-int minutes(int a,int b)
+#include<stdio.h>
+int minutes(int h,int m)
 {
-return (60*a)+b;
+return (60*h)+m;
 }
-
 void main()
 {
 int a,b,result;
-
 printf("\n Enter the number of hours and minutes ");
 scanf("%d %d",&a,&b);
 result=minutes(a,b);
