b0a5670448034a67f4863f66e9f783f859e12f4a
Ayman <84075654+ayman-musk@users.noreply.github.com>
2021-07-21 01:27:31 +0530

    Update distance.c

--- distance.c
+++ distance.c
@@ -4,9 +4,12 @@ void main()
 {
 int x1,x2,y1,y2;
 float d;
-printf("enter x1,x2,y1,y2 values: \n ");
-scanf("%d%d%d%d",&x1,&x2,&y1,&y2);
+printf("Enter x1 and y1 for point 1: \n");
+scanf("%d%d",&x1,&y1);
+printf("Enter x2 and y2 for point 2: \n");
+scanf("%d%d",&x2,&y2);
 d = sqrt(pow((x2-x1),2) + pow((y2 -y1),2));
-printf("the distance between the two points = %f",d);
+printf("The distance between the two points = %f",d);
 }
+
 
