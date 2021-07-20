2b4446cd5ec5b394dfa070fdb3fc49dca5190561
Ayman <84075654+ayman-musk@users.noreply.github.com>
2021-07-21 02:28:01 +0530

    Update marks in 4 subjects.c

--- marks in 4 subjects.c
+++ marks in 4 subjects.c
@@ -1,19 +1,19 @@
 #include<stdio.h>
 void main()
 {
-int marks[4][3],highest[4],i,j;
-for(i=0;i<4;i++)
-{printf("Enter marks in subject %d : \n ",(i+1));
-for(j=0;j<3;j++)
+int marks[3][4],highest[3],i,j;
+for(i=0;i<3;i++)
+{printf("Enter the marks of the students in subject %d:\n",(i+1));
+for(j=0;j<4;j++)
 {scanf("%d",&marks[i][j]);}
 }
-for(i=0;i<4;i++)
+for(i=0;i<3;i++)
 {highest[i]=marks[i][0];
-for(j=1;j<3;j++)
+for(j=1;j<4;j++)
 {if(marks[i][j]>highest[i])
 {highest[i]=marks[i][j];}
 }
 }
-for(i=0;i<4;i++)
-{printf("Highest marks in subject %d = %d \n",(i+1),highest[i]);}
+for(i=0;i<3;i++)
+{printf("Highest marks in subject %d = %d\n",(i+1),highest[i]);}
 }
